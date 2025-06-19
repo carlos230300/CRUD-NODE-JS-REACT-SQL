617da15eb700736af7ebf803ad4d8125eecab7ed
carlos <carllos2303@gmail.com>
2025-06-19 10:36:25 -0300

    Update db.js

--- crud/api/db.js
+++ crud/api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "172300",
-    database: "crud"
+    host: "",
+    user: "",
+    password: "",
+    database: ""
 });
